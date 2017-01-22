ca2228711f3c382749258de22a927e5d330f9ed4
Florian Motlik <flomotlik@gmail.com>
2017-01-22 03:04:13 +0100

    Implement all CF stack attributes and tests

--- formica/template.py
+++ formica/template.py
@@ -8,10 +8,6 @@ import importlib
 import troposphere
 from . import helper
 
-template = Template()
-
-TROPOSPHERE_PATH = os.path.dirname(troposphere.__file__)
-
 DISALLOWED_MODULES = [
     'openstack',
     'template_generator',
@@ -19,6 +15,8 @@ DISALLOWED_MODULES = [
     'helpers',
     'validators',
     'dynamodb2']
+
+TROPOSPHERE_PATH = os.path.dirname(troposphere.__file__)
 
 TROPOSPHERE_MODULE_NAMES = [
     name for _, name, _ in pkgutil.iter_modules([TROPOSPHERE_PATH])
@@ -31,24 +29,36 @@ CLOUDFORMATION_FUNCTIONS = {
     x.__name__: x for x in AWSHelperFn.__subclasses__()}
 
 
-def load(path=os.getcwd(), part='*', variables={}):
-    formica_commands = {
-        'resource': lambda resource: template.add_resource(resource),
-        'mapping': lambda name, values: template.add_mapping(name, values),
-        'module': module,
-        'name': helper.name}
-    toload = f'{path}/{part}.fc'
-    for file in glob.glob(toload):
-        with open(file) as f:
-            code = compile(f.read(), file, 'exec')
-            exec(code,
-                 {**formica_commands,
-                  **CLOUDFORMATION_FUNCTIONS,
-                  **TROPOSPHERE_MODULES,
-                  **variables},
-                 )
+class Template():
 
+    def __init__(self):
+        self.cftemplate = troposphere.Template()
 
-def module(module_name, part='*', **variables):
-    filepath = os.path.dirname(inspect.stack()[1].filename)
-    load(filepath.rstrip('/') + '/' + module_name.strip('/'), part, variables)
+    def template(self):
+        return self.cftemplate.to_json()
+
+    def module(self, module_name, part='*', **variables):
+        filepath = os.path.dirname(inspect.stack()[1].filename)
+        self.load(filepath.rstrip('/') + '/' + module_name.strip('/'), part, variables)
+
+    def load(self, path=os.getcwd(), part='*', variables={}):
+        formica_commands = {
+            'resource': lambda resource: self.cftemplate.add_resource(resource),
+            'mapping': lambda name, values: self.cftemplate.add_mapping(name, values),
+            'description':
+                lambda description: self.cftemplate.add_description(description),
+            'metadata': lambda metadata: self.cftemplate.add_metadata(metadata),
+            'condition':
+                lambda name, condition: self.cftemplate.add_condition(name, condition),
+            'module': self.module,
+            'name': helper.name}
+        toload = f'{path}/{part}.fc'
+        for file in glob.glob(toload):
+            with open(file) as f:
+                code = compile(f.read(), file, 'exec')
+                exec(code,
+                     {**formica_commands,
+                      **CLOUDFORMATION_FUNCTIONS,
+                      **TROPOSPHERE_MODULES,
+                      **variables},
+                     )
--- tests/test_template.py
+++ tests/test_template.py
@@ -5,19 +5,22 @@ import os
 
 import json
 
-from formica import template
+from formica.template import Template
 
 
 class TestTemplate(unittest.TestCase):
 
+    def setUp(self):
+        self.template = Template()
+
     @patch('formica.template.glob')
     def test_load_uses_current_path_as_default(self, glob):
-        template.load()
+        self.template.load()
         glob.glob.assert_called_with(f'{os.getcwd()}/*.fc')
 
     @patch('formica.template.glob')
     def test_load_uses_configured_path_and_module(self, glob):
-        template.load('/some/path', 'module')
+        self.template.load('/some/path', 'module')
         glob.glob.assert_called_with('/some/path/module.fc')
 
     @patch('formica.template.compile')
@@ -26,7 +29,7 @@ class TestTemplate(unittest.TestCase):
     @patch('formica.template.glob')
     def test_opens_globbed_files(self, glob, open, exec, compile):
         glob.glob.return_value = ['some-file']
-        template.load('/some/path', 'module')
+        self.template.load('/some/path', 'module')
         open.assert_called_with('some-file')
 
     @patch('builtins.open', mock_open(
@@ -34,7 +37,51 @@ class TestTemplate(unittest.TestCase):
     @patch('formica.template.glob')
     def test_successfully_adds_resources_to_template(self, glob):
         glob.glob.return_value = ['some-file']
-        template.load('/some/path', 'module')
+        self.template.load('/some/path', 'module')
         expected = {'Resources': {'TestName': {'Type': 'AWS::S3::Bucket'}}}
-        actual = json.loads(template.template.to_json())
+        actual = json.loads(self.template.template())
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.open', mock_open(
+        read_data='description("TestDescription")'))
+    @patch('formica.template.glob')
+    def test_successfully_adds_description_to_template(self, glob):
+        glob.glob.return_value = ['some-file']
+        self.template.load('/some/path', 'module')
+        expected = {'Description': 'TestDescription', 'Resources': {}}
+        actual = json.loads(self.template.template())
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.open', mock_open(
+        read_data='metadata({"key": "value", "key2": "value2"})'))
+    @patch('formica.template.glob')
+    def test_successfully_adds_metadata_to_template(self, glob):
+        glob.glob.return_value = ['some-file']
+        self.template.load('/some/path', 'module')
+        expected = {'Metadata': {'key': 'value', 'key2': 'value2'}, 'Resources': {}}
+        actual = json.loads(self.template.template())
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.open', mock_open(
+        read_data='condition("Condition1", Equals(Ref("EnvType"), "prod"))'))
+    @patch('formica.template.glob')
+    def test_successfully_adds_condition_to_template(self, glob):
+        glob.glob.return_value = ['some-file']
+        self.template.load('/some/path', 'module')
+        expected = {'Conditions':
+                    {'Condition1':
+                        {'Fn::Equals':
+                            [{'Ref': 'EnvType'}, 'prod']}}, 'Resources': {}}
+        actual = json.loads(self.template.template())
+        self.assertEqual(actual, expected)
+
+    @patch('builtins.open', mock_open(
+        read_data='mapping("RegionMap", {"us-east-1": {"AMI": "ami-7f418316"}})'))
+    @patch('formica.template.glob')
+    def test_successfully_adds_mapping_to_template(self, glob):
+        glob.glob.return_value = ['some-file']
+        self.template.load('/some/path', 'module')
+        expected = {'Mappings':
+            {'RegionMap': {'us-east-1': {"AMI": "ami-7f418316"}}}, 'Resources': {}}
+        actual = json.loads(self.template.template())
         self.assertEqual(actual, expected)
