2c91d337208caf6ded6745c954f9450142e1dce1
Florian Motlik <flomotlik@gmail.com>
2017-02-07 19:51:29 +0100

    Implement Parameter support for stack create/update
    fixes #6

--- tests/test_new.py
+++ tests/test_new.py
@@ -27,5 +27,30 @@ class TestNew(unittest.TestCase):
         loader.return_value.template.return_value = TEMPLATE
         self.run_create()
         change_set.assert_called_with(stack=STACK, client=client_mock)
-        change_set.return_value.create.assert_called_once_with(template=TEMPLATE, type='CREATE')
+        change_set.return_value.create.assert_called_once_with(template=TEMPLATE, type='CREATE', parameters={})
         change_set.return_value.describe.assert_called_once()
+
+    @patch('formica.cli.Loader')
+    @patch('formica.helper.AWSSession')
+    @patch('formica.cli.ChangeSet')
+    def test_new_uses_parameters_for_creation(self, change_set, session, loader):
+        client_mock = Mock()
+        session.return_value.client_for.return_value = client_mock
+        loader.return_value.template.return_value = TEMPLATE
+        runner = CliRunner()
+        result = runner.invoke(cli.new,
+                               ['--stack', STACK, '--parameter', 'A=B', '--profile', PROFILE, '--region', REGION,
+                                '--parameter', 'C=D'])
+        self.assertEqual(result.exit_code, 0)
+        change_set.assert_called_with(stack=STACK, client=client_mock)
+        change_set.return_value.create.assert_called_once_with(template=TEMPLATE, type='CREATE',
+                                                               parameters={'A': 'B', 'C': 'D'})
+        change_set.return_value.describe.assert_called_once()
+
+    def test_new_tests_parameter_format(self):
+        runner = CliRunner()
+        result = runner.invoke(cli.new,
+                               ['--stack', STACK, '--parameter', 'A=B', '--profile', PROFILE, '--region', REGION,
+                                '--parameter', 'CD'])
+        self.assertIn('parameters need to be in format KEY=VALUE', result.output)
+        self.assertEqual(result.exit_code, 2)
