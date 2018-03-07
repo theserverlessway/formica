a7448bce6e464cba4658d79da740ba52ddbfec81
Florian Motlik <flo@flomotlik.me>
2018-03-07 14:22:47 +0100

    Update all dependencies

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ setup(
     ],
     keywords='cloudformation, aws, cloud',
     packages=['formica'],
-    install_requires=['boto3>=1.4.4,<2.0.0', 'texttable==0.8.7', 'jinja2==2.9.5', 'pyyaml==3.12',
-                      'deepdiff==3.1.2'],
+    install_requires=['boto3==1.6.4', 'texttable==1.2.1', 'jinja2==2.10', 'pyyaml==3.12',
+                      'deepdiff==3.3.0'],
     entry_points={
         'console_scripts': [
             'formica=formica.cli:formica',
