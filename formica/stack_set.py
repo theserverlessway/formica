c0cd213d17eba947c630b5537ac526991461efc3
Florian Motlik <flo@flomotlik.me>
2018-10-02 10:46:29 +0200

    Add execution/admin role and expect aws regions

--- formica/stack_set.py
+++ formica/stack_set.py
@@ -1,24 +1,51 @@
 from formica.aws import AWS
 from .loader import Loader
 import logging
+import sys
 
 logger = logging.getLogger(__name__)
 
 
+def requires_stack_set(function):
+    def validate_stack_set(args):
+        if not args.stack_set:
+            logger.error('You need to set the stack set either with --stack-set(-s) or '
+                         'in a config file set with --config-file(-c)')
+            sys.exit(1)
+        else:
+            function(args)
+    return validate_stack_set
+
+
+def requires_accounts_regions(function):
+    def validate_stack_set(args):
+        if args.accounts and args.regions:
+            function(args)
+        else:
+            logger.error('You need to set the regions and accounts you want to update.')
+            sys.exit(1)
+    return validate_stack_set
+
+
+@requires_stack_set
 def update_stack_set(args):
     __manage_stack_set(args=args, create=False)
 
 
+@requires_stack_set
 def create_stack_set(args):
     __manage_stack_set(args=args, create=True)
 
 
+@requires_stack_set
 def remove_stack_set(args):
     client = AWS.current_session().client('cloudformation')
     client.delete_stack_set(StackSetName=args.stack_set)
     logger.info('Removed StackSet with name {}'.format(args.stack_set))
 
 
+@requires_stack_set
+@requires_accounts_regions
 def add_stack_set_instances(args):
     client = AWS.current_session().client('cloudformation')
     client.create_stack_instances(StackSetName=args.stack_set,
@@ -27,6 +54,8 @@ def add_stack_set_instances(args):
     logger.info('Added StackSet Instances for StackSet {}'.format(args.stack_set))
 
 
+@requires_stack_set
+@requires_accounts_regions
 def remove_stack_set_instances(args):
     client = AWS.current_session().client('cloudformation')
     client.delete_stack_instances(StackSetName=args.stack_set,
@@ -67,10 +96,10 @@ def parameters(parameters, tags, capabilities, accounts, regions, execution_role
     optional_arguments = {}
     if parameters:
         optional_arguments['Parameters'] = [
-            {'ParameterKey': key, 'ParameterValue': value, 'UsePreviousValue': False} for (key, value)
+            {'ParameterKey': key, 'ParameterValue': str(value), 'UsePreviousValue': False} for (key, value)
             in parameters.items()]
     if tags:
-        optional_arguments['Tags'] = [{'Key': key, 'Value': value, } for (key, value) in
+        optional_arguments['Tags'] = [{'Key': key, 'Value': str(value)} for (key, value) in
                                       tags.items()]
     if capabilities:
         optional_arguments['Capabilities'] = capabilities
