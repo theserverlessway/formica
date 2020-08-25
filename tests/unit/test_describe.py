7698ec18abd25ed41b3104a382e7d8ca38d755ca
Florian Motlik <flo@theserverlessway.com>
2020-08-25 18:44:22 +0200

    Remove Assert Call not necessary anymore

--- tests/unit/test_describe.py
+++ tests/unit/test_describe.py
@@ -7,6 +7,5 @@ from tests.unit.constants import STACK
 
 def test_describes_change_set(boto_client, change_set):
     cli.main(['describe', '--stack', STACK])
-    boto_client.assert_called_with('cloudformation')
     change_set.assert_called_with(stack=STACK)
     change_set.return_value.describe.assert_called_once()
